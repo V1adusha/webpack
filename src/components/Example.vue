7e9a3b703a0252735e7a78f780583f238f3b58d3
Vladislav <vlad.umarov99@gmail.com>
2019-06-20 00:18:49 +0300

    start

---
+++ src/components/Example.vue
@@ -0,0 +1,20 @@
+<template>
+	<section>
+		<div class="container">
+			<img src="/assets/img/logo.png" alt="message">
+			<p>{{ message }}</p>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			message: 'Example Vue component',
+		}
+	}
+}
+</script>
+
+
